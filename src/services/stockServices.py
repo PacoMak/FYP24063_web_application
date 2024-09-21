54dfe4feacf4ff4322c84c6baf3f5a241509e5c3
PacoMak <makwansing@gmail.com>
2024-09-22 00:35:01 +0800

    feat: numpy format for stock data

--- src/services/stockServices.py
+++ src/services/stockServices.py
@@ -3,8 +3,12 @@ import yfinance as yf
 supportStock = ["MSFT", "TSLA", "CEG", "VST", "GME", "APGE", "MASI"]
 
 
-def getSingleStock(stockName):
+def getSingleStock(stockName, format="json"):
     stock = yf.Ticker(stockName)
     hist = stock.history(period="1mo", interval="5m")
     hist.reset_index(inplace=True)
-    return hist.to_json(orient="records", lines=False)
+    if format == "json":
+        return hist.to_json(orient="records", lines=False)
+    else:
+        # for model trainning
+        return hist.to_numpy()
