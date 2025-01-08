ef726ee7b81db118c1a1cb1ab7de9a1551b4de65
PacoMak <makwansing@gmail.com>
2025-01-08 22:08:12 +0800

    feat: stock info

--- backend/src/controllers/stock.py
+++ backend/src/controllers/stock.py
@@ -1,23 +1,16 @@
 from flask import Blueprint, Response, json, request
-from ..services import getStocksData, supportStock, getStockData
+from ..services import stock_services
 
 stock = Blueprint("stock", __name__)
 
 
-@stock.route("/stockList", methods=["GET"])
-def getStockList():
+@stock.route("/stocks/history", methods=["GET"])
+def get_stocks_history():
     try:
         tickers_list = request.args.getlist("tickers")
         if not tickers_list:
             return Response(status="401")
-        for ticker in tickers_list:
-            if ticker not in supportStock:
-                return Response(
-                    response=f"{ticker} is not supported",
-                    status=401,
-                    mimetype="application/json",
-                )
-        stockData = getStocksData(tickers_list)
+        stockData = stock_services.get_stocks_data(tickers_list)
         json_data = {ticker: json.loads(data) for ticker, data in stockData.items()}
         return Response(
             response=json.dumps(json_data), status=200, mimetype="application/json"
@@ -26,22 +19,41 @@ def getStockList():
         return Response(response="internal error", status=501)
 
 
-@stock.route("/stock", methods=["GET"])
-def getStock():
+@stock.route("/stock/history", methods=["GET"])
+def get_stock_history():
     try:
         ticker = request.args.get("ticker")
         if not ticker:
             return Response(status="401")
-        if ticker not in supportStock:
-            return Response(
-                response=f"{ticker} is not supported",
-                status=401,
-                mimetype="application/json",
-            )
-        stockData = getStockData(ticker)
+        stockData = stock_services.get_stock_data(ticker)
         return Response(
             response=json.dumps(
                 json.loads(stockData) if isinstance(stockData, str) else stockData
+            ),
+            status=200,
+            mimetype="application/json",
+        )
+    except:
+        return Response(response="internal error", status=501)
+
+
+@stock.route("/stock/info", methods=["GET"])
+def get_stock_info():
+    try:
+        print(1)
+        ticker = request.args.get("ticker")
+        print(2)
+        if not ticker:
+            return Response(status="401")
+
+        print(3)
+        print(ticker)
+        stock_info = stock_services.get_stock_info(ticker)
+        print(4)
+
+        return Response(
+            response=json.dumps(
+                json.loads(stock_info) if isinstance(stock_info, str) else stock_info
             ),
             status=200,
             mimetype="application/json",
