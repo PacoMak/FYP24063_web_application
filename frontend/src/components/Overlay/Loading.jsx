be636d16d649ecdca5bccf6f66ed04fd9e612226
PacoMak <makwansing@gmail.com>
2025-04-04 01:03:53 +0800

    delete case insensitive file

--- frontend/src/components/Overlay/Loading.jsx
+++
@@ -1,8 +0,0 @@
-import { CircularProgress } from "@mui/material";
-import { memo } from "react";
-
-export const Loading = memo(() => {
-  return <CircularProgress color="inherit" />;
-});
-
-Loading.displayName = "Loading";
